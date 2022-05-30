d36c96cfb9784de1906d9d85a61917664288e25f
lcnr <rust@lcnr.de>
2022-05-30 21:30:34 +0200

    w

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
+#![allow(unused)]
+
 use bicliques::*;
+
+use std::collections::HashSet;
+use std::ops::ControlFlow;
 
 fn from_str(s: &str) -> Bigraph {
     let mut iter = s.split_whitespace();
     let left = iter.next().unwrap().parse::<u32>().unwrap();
     let right = iter.next().unwrap().parse::<u32>().unwrap();
     let mut g = Bigraph::new(left, right);
-    for y in 0..right {
-        for x in 0..left {
+    for x in 0..left {
+        for y in 0..right {
             match iter.next().unwrap() {
                 "1" => g.add(Entry(x, y)),
-                "0" => (),
+                "_" => (),
                 e => panic!("unexpected: {}", e),
             }
         }
@@ -20,20 +25,125 @@ fn from_str(s: &str) -> Bigraph {
     g
 }
 
-const INPUT: &'static str = "5 5
-    1 1 1 1 0
-    1 1 0 1 1
-    1 0 1 1 0
-    1 1 1 1 1
-    0 1 0 1 1
+const EX0: &str = "5 5
+1 1 1 1 _
+1 1 _ 1 1
+1 _ 1 1 _
+1 1 1 1 1
+_ 1 _ 1 1
+";
+
+const EX1: &str = "7 7
+1 1 1 1 1 _ 1
+1 1 1 1 _ 1 1
+1 1 1 _ 1 1 1
+1 1 _ 1 1 1 1
+1 _ 1 1 1 1 1
+_ 1 1 1 1 1 1
+1 1 1 1 1 1 1
+";
+
+const EX2: &str = "6 6
+_ 1 1 1 1 1
+1 _ 1 1 1 1
+1 1 1 1 1 _
+1 1 1 _ 1 1
+1 1 1 1 _ 1
+1 1 _ 1 1 1
+";
+
+const EX3: &str = "6 7
+1 1 1 1 _ 1 1
+1 1 1 _ 1 1 1
+1 1 1 1 1 1 _
+1 _ 1 1 1 1 1
+1 1 1 1 1 _ 1
+_ _ 1 1 1 1 1
+";
+
+const EX4: &str = "6 6
+1 1 _ 1 1 1
+1 _ 1 1 1 1
+1 1 1 1 _ 1
+_ 1 1 1 1 1
+1 1 1 1 1 _
+1 1 1 _ 1 1
+";
+
+const EX5: &str = "3 2
+1 1
+_ 1
+1 _
+";
+
+const EX6: &str = "7 11
+1 1 1 1 1 1 _ _ 1 1 1
+1 1 1 1 _ 1 1 1 _ 1 1
+1 1 1 1 1 _ 1 1 1 1 _
+1 1 _ 1 1 1 1 1 _ 1 1
+1 1 1 1 1 1 1 1 1 _ _
+1 1 1 _ 1 1 1 _ 1 1 1
+_ 1 1 1 _ 1 1 1 _ 1 1
+";
+
+const EX7: &str = "14 10
+1 1 1 _ 1 1 1 _ 1 1 1 _ _ 1
+1 _ _ 1 1 1 _ 1 1 1 1 1 _ 1
+_ _ 1 1 1 _ 1 _ 1 1 1 _ 1 _
+1 1 1 _ 1 _ _ _ 1 1 1 1 1 _
+1 1 1 _ 1 1 _ _ _ _ 1 _ 1 1
+1 _ _ 1 _ _ _ _ 1 1 1 _ _ 1
+_ 1 1 1 1 1 1 1 1 _ _ 1 1 _
+1 _ 1 1 1 1 1 _ _ 1 1 _ _ 1
+1 1 _ _ _ 1 1 _ 1 1 _ _ 1 1
+1 1 1 1 1 _ 1 _ _ 1 1 1 1 1
+";
+
+const EX8: &str = "7 7
+1 1 1 1 1 1 _
+1 1 1 1 1 _ 1
+1 1 1 1 _ 1 1
+1 1 1 _ 1 1 1
+1 1 _ 1 1 1 1
+1 _ 1 1 1 1 1
+_ 1 1 1 1 1 1
+";
+
+const EX9: &str = "6 6
+1 1 1 1 1 _
+1 1 1 1 _ 1
+1 1 1 _ 1 1
+1 1 _ 1 1 1
+1 _ 1 1 1 1
+_ 1 1 1 1 1
+";
+
+const EX10: &str = "6 6
+1 1 1 1 1 _
+1 1 1 1 _ 1
+1 _ 1 _ _ _
+1 1 _ 1 _ 1
+1 _ 1 1 1 1
+_ 1 1 1 1 1
 ";
 
 fn main() {
-    let g = from_str(INPUT);
+    let g = from_str(EX7);
 
     println!("{:?}", g);
 
-    for c in bicliques::biclique_covers(&g, 3) {
-        println!("{:?}", c);
-    }
+    let mut i = 0;
+    let mut cliques = HashSet::new();
+    enum Never {}
+    print!("[");
+    bicliques::biclique_covers::<Never, _>(&g, 13, |mut c| {
+        if g.is_maximal_cover(&c) {
+            println!("{}", c.print(&g));
+            assert!(cliques.insert(c));
+            i += 1;
+        }
+        ControlFlow::Continue(())
+    });
+    print!("]");
+    println!("{}", i);
 }
