abeb96665f80c95754a675b149898de68a7ae486
lcnr <rust@lcnr.de>
2022-07-16 20:28:43 +0200

    up to 2x perf increase

--- src/bin/random.rs
+++ src/bin/random.rs
@@ -1,29 +1,42 @@
 use std::time::Instant;
 
-use bicliques::{forced::forced_elements, Bigraph, Entry};
+use bicliques::{forced::*, Bigraph, Entry};
 use rand::{distributions::Bernoulli, distributions::Distribution};
 
 fn main() {
     let rng = &mut rand::thread_rng();
-    let d = Bernoulli::new(0.5).unwrap();
 
     for x in 10.. {
         for y in 10..=x {
-            let mut graph = Bigraph::new(x, y);
-            for x_pos in 0..x {
-                for y_pos in 0..y {
-                    if d.sample(rng) {
-                        graph.add(Entry(x_pos, y_pos));
+            for p in [0.1, 0.3, 0.5, 0.7, 0.9] {
+                let d = Bernoulli::new(p).unwrap();
+                let mut new_time = 0.0;
+                let mut old_time = 0.0;
+                for _ in 0..100 {
+                    let mut graph = Bigraph::new(x, y);
+                    for x_pos in 0..x {
+                        for y_pos in 0..y {
+                            if d.sample(rng) {
+                                graph.add(Entry(x_pos, y_pos));
+                            }
+                        }
                     }
-                }
-            }
 
-            let now = Instant::now();
-            let brute_size_new = forced_elements(&graph).len();
-            println!(
-                "({x}, {y}): {brute_size_new} in {}",
-                now.elapsed().as_secs_f64()
-            );
+                    let now = Instant::now();
+                    let brute_size_old = forced_elements_old(&graph).len();
+                    old_time += now.elapsed().as_secs_f64();
+
+                    let now = Instant::now();
+                    let brute_size_new = forced_elements(&graph).len();
+                    new_time += now.elapsed().as_secs_f64();
+                    assert_eq!(brute_size_old, brute_size_new);
+                }
+
+                let ratio = new_time / old_time;
+                println!(
+                    "{x}x{y} with p={p}: old={old_time:>7.3}, new={new_time:>7.3}, ratio={ratio}"
+                );
+            }
         }
     }
 }
--- src/forced.rs
+++ src/forced.rs
@@ -1,5 +1,43 @@
 use crate::*;
 
+pub fn forced_elements_old(g: &Bigraph) -> Vec<Entry> {
+    let mut best = Vec::new();
+    recur_old(g, &mut Vec::new(), &mut best, g.entries.clone());
+    best
+}
+
+fn recur_old(
+    g: &Bigraph,
+    chosen: &mut Vec<Entry>,
+    best: &mut Vec<Entry>,
+    mut possible: TBitSet<usize>,
+) {
+    if let Some(first) = possible.iter().next() {
+        // First recur_old while choosing `first`, then without.
+        possible.remove(first);
+        let f = g.entry_from_index(first);
+
+        chosen.push(f);
+        let new_possible = possible
+            .iter()
+            .filter(|&e| {
+                let e = g.entry_from_index(e);
+                !g.may_share(e, f)
+            })
+            .collect();
+        recur_old(g, chosen, best, new_possible);
+        chosen.pop();
+
+        if best.len() < chosen.len() + possible.element_count() {
+            recur_old(g, chosen, best, possible);
+        }
+    } else {
+        if chosen.len() > best.len() {
+            best.clone_from(chosen);
+        }
+    }
+}
+
 pub fn forced_elements(g: &Bigraph) -> Vec<Entry> {
     let mut best = Vec::new();
     recur(g, &mut Vec::new(), &mut best, g.entries.clone());
@@ -12,6 +50,10 @@ fn recur(
     best: &mut Vec<Entry>,
     mut possible: TBitSet<usize>,
 ) {
+    if best.len() >= chosen.len() + possible.element_count() {
+        return;
+    }
+
     if let Some(first) = possible.iter().next() {
         // First recur while choosing `first`, then without.
         possible.remove(first);
@@ -28,9 +70,7 @@ fn recur(
         recur(g, chosen, best, new_possible);
         chosen.pop();
 
-        if best.len() < chosen.len() + possible.element_count() {
-            recur(g, chosen, best, possible);
-        }
+        recur(g, chosen, best, possible);
     } else {
         if chosen.len() > best.len() {
             best.clone_from(chosen);
