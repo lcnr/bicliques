ade0b79a43e629232ae2de0523b214b83a940dd1
lcnr <rust@lcnr.de>
2022-07-16 20:46:28 +0200

    update old

--- src/forced.rs
+++ src/forced.rs
@@ -2,44 +2,52 @@ use crate::*;
 
 pub mod old {
     use crate::*;
+
     pub fn forced_elements(g: &Bigraph) -> Vec<Entry> {
         let mut best = Vec::new();
         let mapping: Vec<_> = g.entries().collect();
+        let mut visibility = Vec::new();
+        for &e in &mapping {
+            let others = (0..mapping.len())
+                .filter(|&o| !g.may_share(e, mapping[o]))
+                .collect();
+            visibility.push(others);
+        }
         let cx = Cx {
-            g,
             mapping: &mapping,
+            visibility: &visibility,
         };
         recur(cx, &mut Vec::new(), &mut best, (0..mapping.len()).collect());
         best
     }
-    
+
     #[derive(Clone, Copy)]
     struct Cx<'x> {
-        g: &'x Bigraph,
         mapping: &'x [Entry],
+        // Stores all entries not seen by `index`.
+        visibility: &'x [TBitSet<usize>],
     }
-    
-    fn recur(cx: Cx<'_>, chosen: &mut Vec<Entry>, best: &mut Vec<Entry>, mut possible: TBitSet<usize>) {
+
+    fn recur(
+        cx: Cx<'_>,
+        chosen: &mut Vec<Entry>,
+        best: &mut Vec<Entry>,
+        mut possible: TBitSet<usize>,
+    ) {
         if best.len() >= chosen.len() + possible.element_count() {
             return;
         }
-    
+
         if let Some(first) = possible.iter().next() {
             // First recur while choosing `first`, then without.
             possible.remove(first);
             let f = cx.mapping[first];
-    
+
             chosen.push(f);
-            let new_possible = possible
-                .iter()
-                .filter(|&e| {
-                    let e = cx.mapping[e];
-                    !cx.g.may_share(e, f)
-                })
-                .collect();
+            let new_possible = possible.intersection(&cx.visibility[first]);
             recur(cx, chosen, best, new_possible);
             chosen.pop();
-    
+
             recur(cx, chosen, best, possible);
         } else {
             if chosen.len() > best.len() {
@@ -47,8 +55,6 @@ pub mod old {
             }
         }
     }
-    
-
 }
 
 pub fn forced_elements(g: &Bigraph) -> Vec<Entry> {
@@ -56,7 +62,9 @@ pub fn forced_elements(g: &Bigraph) -> Vec<Entry> {
     let mapping: Vec<_> = g.entries().collect();
     let mut visibility = Vec::new();
     for &e in &mapping {
-        let others = (0..mapping.len()).filter(|&o| !g.may_share(e, mapping[o])).collect();
+        let others = (0..mapping.len())
+            .filter(|&o| !g.may_share(e, mapping[o]))
+            .collect();
         visibility.push(others);
     }
     let cx = Cx {
